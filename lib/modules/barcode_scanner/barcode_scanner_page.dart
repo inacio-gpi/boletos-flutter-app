e16f30c7c4f79d692f2008a5d44491e681d2d3f5
Guilherme Pereira <okkopsiu@gmail.com>
2021-10-15 14:48:17 -0300

    add: Layout completo e Funcionalidades

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -4,7 +4,7 @@ import 'package:boletos_flutter_app/modules/barcode_scanner/barcode_scanner_stat
 import 'package:boletos_flutter_app/shared/themes/app_colors.dart';
 import 'package:boletos_flutter_app/shared/themes/app_text_styles.dart';
 import 'package:boletos_flutter_app/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
-import 'package:boletos_flutter_app/shared/widgets/set_buttons/set_label_buttons.dart';
+import 'package:boletos_flutter_app/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   BarcodeScannerPage({Key? key}) : super(key: key);
@@ -21,10 +21,10 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
     controller.getAvailableCameras();
     controller.statusNotifier.addListener(() {
       if (controller.status.hasBarcode) {
-        Navigator.pushReplacementNamed(context, "/insert_boleto");
+        Navigator.pushReplacementNamed(context, "/insert_boleto",
+            arguments: controller.status.barcode);
       }
     });
-
     super.initState();
   }
 
@@ -44,28 +44,28 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
       child: Stack(
         children: [
           ValueListenableBuilder<BarcodeScannerStatus>(
-              valueListenable: controller.statusNotifier,
-              builder: (_, status, __) {
-                if (status.showCamera) {
-                  return Container(
-                    color: Colors.blue,
-                    child: controller.cameraController!.buildPreview(),
-                  );
-                } else {
-                  return Container();
-                }
-              }),
+            valueListenable: controller.statusNotifier,
+            builder: (_, status, __) {
+              if (status.showCamera) {
+                return Container(
+                  child: controller.cameraController!.buildPreview(),
+                );
+              } else {
+                return Container();
+              }
+            },
+          ),
           RotatedBox(
             quarterTurns: 1,
             child: Scaffold(
                 backgroundColor: Colors.transparent,
                 appBar: AppBar(
                   backgroundColor: Colors.black,
-                  centerTitle: true,
                   title: Text(
                     "Escaneie o código de barras do boleto",
                     style: TextStyles.buttonBackground,
                   ),
+                  centerTitle: true,
                   leading: BackButton(
                     color: AppColors.background,
                   ),
@@ -73,53 +73,50 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                 body: Column(
                   children: [
                     Expanded(
-                      child: Container(
-                        color: Colors.black,
-                      ),
-                    ),
+                        child: Container(
+                      color: Colors.black.withOpacity(0.6),
+                    )),
                     Expanded(
-                      flex: 2,
-                      child: Container(
-                        color: Colors.transparent,
-                      ),
-                    ),
+                        flex: 2,
+                        child: Container(
+                          color: Colors.transparent,
+                        )),
                     Expanded(
-                      child: Container(
-                        color: Colors.black,
-                      ),
-                    )
+                        child: Container(
+                      color: Colors.black.withOpacity(0.6),
+                    ))
                   ],
                 ),
                 bottomNavigationBar: SetLabelButtons(
-                  labelPrimary: "Inserir código do boleto",
-                  onTapPrimary: () {
-                    controller.status = BarcodeScannerStatus.error("Error");
-                  },
-                  labelSecondary: "Adicionar da galeria",
-                  onTapSecondary: controller.scanWithImagePicker,
-                )),
+                    primaryLabel: "Inserir código do boleto",
+                    primaryOnPressed: () {
+                      Navigator.pushReplacementNamed(context, "/insert_boleto");
+                    },
+                    secondaryLabel: "Adicionar da galeria",
+                    secondaryOnPressed: () {})),
           ),
           ValueListenableBuilder<BarcodeScannerStatus>(
-              valueListenable: controller.statusNotifier,
-              builder: (_, status, __) {
-                if (status.hasError) {
-                  return Align(
-                      alignment: Alignment.bottomLeft,
-                      child: BottomSheetWidget(
-                          labelPrimary: "Escanear novamente",
-                          onTapPrimary: () {
-                            controller.scanWithCamera();
-                          },
-                          labelSecondary: "Digitar código",
-                          onTapSecondary: () {},
-                          title:
-                              "Não foi possível identificar um código de barras.",
-                          subtitle:
-                              "Tente escanear novamente ou digite o código do seu boleto."));
-                } else {
-                  return Container();
-                }
-              }),
+            valueListenable: controller.statusNotifier,
+            builder: (_, status, __) {
+              if (status.hasError) {
+                return BottomSheetWidget(
+                  title: "Não foi possível identificar um código de barras.",
+                  subtitle:
+                      "Tente escanear novamente ou digite o código do seu boleto.",
+                  primaryLabel: "Escanear novamente",
+                  primaryOnPressed: () {
+                    controller.scanWithCamera();
+                  },
+                  secondaryLabel: "Digitar código",
+                  secondaryOnPressed: () {
+                    Navigator.pushReplacementNamed(context, "/insert_boleto");
+                  },
+                );
+              } else {
+                return Container();
+              }
+            },
+          ),
         ],
       ),
     );
--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:boletos_flutter_app/modules/extract/extract_page.dart';
 import 'package:boletos_flutter_app/modules/home/home_controller.dart';
+import 'package:boletos_flutter_app/modules/meus_boletos/meus_boletos_page.dart';
+import 'package:boletos_flutter_app/shared/models/user_model.dart';
 import 'package:boletos_flutter_app/shared/themes/app_colors.dart';
 import 'package:boletos_flutter_app/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final UserModel user;
+  const HomePage({Key? key, required this.user}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,12 +16,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final controller = HomeController();
-  final pages = [
-    Container(
-      color: Colors.red,
-    ),
-    Container(color: Colors.blue),
-  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +33,8 @@ class _HomePageState extends State<HomePage> {
                     style: TextStyles.titleRegular,
                     children: [
                       TextSpan(
-                          text: "Psiu", style: TextStyles.titleBoldBackground)
+                          text: "${widget.user.name}",
+                          style: TextStyles.titleBoldBackground)
                     ]),
               ),
               subtitle: Text(
@@ -46,13 +46,22 @@ class _HomePageState extends State<HomePage> {
                 width: 48,
                 decoration: BoxDecoration(
                     color: Colors.black,
-                    borderRadius: BorderRadius.circular(5)),
+                    borderRadius: BorderRadius.circular(5),
+                    image: DecorationImage(
+                        image: NetworkImage(widget.user.photoURL!))),
               ),
             ),
           ),
         ),
       ),
-      body: pages[controller.currentPage],
+      body: [
+        MeusBoletosPage(
+          key: UniqueKey(),
+        ),
+        ExtractPage(
+          key: UniqueKey(),
+        )
+      ][controller.currentPage],
       bottomNavigationBar: Container(
         height: 90,
         child: Row(
@@ -65,11 +74,14 @@ class _HomePageState extends State<HomePage> {
                 },
                 icon: Icon(
                   Icons.home,
-                  color: AppColors.primary,
+                  color: controller.currentPage == 0
+                      ? AppColors.primary
+                      : AppColors.body,
                 )),
             GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, "/barcode_scanner");
+              onTap: () async {
+                await Navigator.pushNamed(context, "/barcode_scanner");
+                setState(() {});
               },
               child: Container(
                 width: 56,
@@ -90,7 +102,9 @@ class _HomePageState extends State<HomePage> {
                 },
                 icon: Icon(
                   Icons.description_outlined,
-                  color: AppColors.body,
+                  color: controller.currentPage == 1
+                      ? AppColors.primary
+                      : AppColors.body,
                 ))
           ],
         ),
